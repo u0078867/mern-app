2e00fe472b9ff74c64bc0ea4fbce433163deb59c
Davide Monari <davide.monari@kuleuven.be>
2017-08-04 11:07:13 +0200

    test

--- server/app/controllers/database.controller.js
+++ server/app/controllers/database.controller.js
@@ -15,24 +15,25 @@ export function getCollections(req, res) {
       return res.status(500).send(err);
     }
     // Filter out internal use collections
-    //console.log(colls);
+    console.log(colls);
     try {
-      var collections = colls.filter(c => !['forms','subms'].includes(c.name));
+      //var collections = colls.filter(c => !['forms','subms'].includes(c.name));
+      var collections = colls.filter(c => ['subjects','researchers','devices','swtools','activities'].includes(c.name));
       let models = mongoose.connection.models;
       //console.log('models:');
       //console.log(models);
       collections = collections.map(c => {
         // Search corresponding model
-        console.log('model:');
+        //console.log('model:');
         let model = Object.keys(models).filter(m => {
-          console.log(models[m].collection.collectionName);
+          //console.log(models[m].collection.collectionName);
           return models[m].collection.collectionName == c.name
         })[0];
-        console.log('model found:');
-        console.log(model);
+        //console.log('model found:');
+        //console.log(model);
         const Model = models[model];
-        console.log('model class extracted');
-        console.log(Model);
+        //console.log('model class extracted');
+        //console.log(Model);
         // Add merge wented data from schema and model
         return {
           name: c.name,
