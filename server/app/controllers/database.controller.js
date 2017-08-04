8c7c16a403be8ca77c28f4f1e672220e77293688
Davide Monari <davide.monari@kuleuven.be>
2017-08-04 10:38:40 +0200

    test

--- server/app/controllers/database.controller.js
+++ server/app/controllers/database.controller.js
@@ -17,14 +17,20 @@ export function getCollections(req, res) {
     // Filter out internal use collections
     console.log(colls);
     try {
-      collections = colls.filter(c => !['forms','subms'].includes(c.name));
+      var collections = colls.filter(c => !['forms','subms'].includes(c.name));
       let models = mongoose.connection.models;
+      console.log('models:');
+      console.log(models);
       collections = collections.map(c => {
         // Search corresponding model
+        console.log('model:');
         let model = Object.keys(models).filter(m => {
           return models[m].collection.collectionName == c.name
         })[0];
+        console.log('model found');
         const Model = models[model];
+        console.log('model class extracted');
+        console.log(Model);
         // Add merge wented data from schema and model
         return {
           name: c.name,
