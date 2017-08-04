b042593df21a3acf1059183d01333b0f6580e2fd
Davide Monari <davide.monari@kuleuven.be>
2017-08-04 11:16:27 +0200

    fixed api/database/collections for new mongoose version

--- server/app/controllers/database.controller.js
+++ server/app/controllers/database.controller.js
@@ -7,33 +7,22 @@ import mongoose from 'mongoose';
  * @returns void
  */
 export function getCollections(req, res) {
-  //console.log(mongoose.connection.models)
-  try {
   mongoose.connection.db.listCollections().toArray((err, colls) => {
     if (err) {
       console.log(err);
       return res.status(500).send(err);
     }
     // Filter out internal use collections
-    console.log(colls);
     try {
       //var collections = colls.filter(c => !['forms','subms'].includes(c.name));
       var collections = colls.filter(c => ['subjects','researchers','devices','swtools','activities'].includes(c.name));
       let models = mongoose.connection.models;
-      //console.log('models:');
-      //console.log(models);
       collections = collections.map(c => {
         // Search corresponding model
-        //console.log('model:');
         let model = Object.keys(models).filter(m => {
-          //console.log(models[m].collection.collectionName);
           return models[m].collection.collectionName == c.name
         })[0];
-        //console.log('model found:');
-        //console.log(model);
         const Model = models[model];
-        //console.log('model class extracted');
-        //console.log(Model);
         // Add merge wented data from schema and model
         return {
           name: c.name,
@@ -45,7 +34,4 @@ export function getCollections(req, res) {
       return res.status(500).send(err);
     }
   });
-  } catch (err) {
-    return res.status(500).send(err);
-  }
 }
